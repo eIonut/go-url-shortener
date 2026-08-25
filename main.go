1ba10b244ef185dbb4460aa5fa27cb3e415dd95e
Gabriel Ionut Enache <enacheionut2399@gmail.com>
2026-08-25 21:31:13 +0300

    add graceful shutdown

--- main.go
+++ main.go
@@ -7,16 +7,21 @@ import (
 	"go-url-shortener/config"
 	"go-url-shortener/database/db"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/jackc/pgx/v5"
 )
 
-func startServer(server *http.Server, port string) {
-	fmt.Println("Server listening on port:", port)
+func startServer(server *http.Server, serverErr chan<- error) {
+	fmt.Println("Server listening on port:", server.Addr)
 
 	err := server.ListenAndServe()
+
 	if err != nil && err != http.ErrServerClosed {
-		fmt.Println("Server error:", err)
+		serverErr <- err
 	}
 }
 
@@ -30,13 +35,45 @@ func main() {
 	defer conn.Close(context.Background())
 
 	queries := db.New(conn)
-
 	handler := api.NewHandler(queries)
 	router := api.NewRouter(handler)
-
 	server := &http.Server{
 		Addr:    ":" + cfg.Port,
 		Handler: router,
 	}
-	startServer(server, cfg.Port)
+
+	ctx, stop := signal.NotifyContext(
+		context.Background(),
+		os.Interrupt,
+		syscall.SIGTERM,
+	)
+
+	defer stop()
+
+	serverErr := make(chan error, 1)
+
+	go startServer(server, serverErr)
+
+	select {
+	case <-ctx.Done():
+		fmt.Println("shutdown signal received")
+
+	case err := <-serverErr:
+		fmt.Println("server error:", err)
+		return
+	}
+
+	shutdownCtx, cancel := context.WithTimeout(
+		context.Background(),
+		5*time.Second,
+	)
+
+	defer cancel()
+
+	fmt.Println("Server shutting down...")
+
+	err = server.Shutdown(shutdownCtx)
+	if err != nil {
+		fmt.Println("shutdown error:", err)
+	}
 }
